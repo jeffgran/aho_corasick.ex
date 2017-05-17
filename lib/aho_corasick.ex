345448dc99938023abe4c02e309da845dc33e014
Jeff Gran <jeff.gran@gmail.com>
2017-05-17 00:26:56 -0600

    pre-compute fail edges

--- lib/aho_corasick.ex
+++ lib/aho_corasick.ex
@@ -3,10 +3,13 @@ defmodule AhoCorasick do
 
   defstruct graph: nil
 
-  def new do
+  def new(terms) do
     g = :digraph.new()
     :digraph.add_vertex(g, :root, :root)
-    %AhoCorasick{graph: g}
+    ac = %AhoCorasick{graph: g}
+    Enum.each terms, &add_term(ac, &1)
+    build_trie(ac)
+    ac
   end
 
   def add_term(ac, term) do
@@ -43,15 +46,125 @@ defmodule AhoCorasick do
 
   def add_token(ac, token, node, edge \\ nil) do
     v = :digraph.add_vertex(ac.graph)
-    v = :digraph.add_vertex(ac.graph, v, token)
-    #debug "adding vertex for #{token}"
-    #debug v
+    v = :digraph.add_vertex(ac.graph, v)
 
     if edge == nil do
-      :digraph.add_edge(ac.graph, node, v, token)
+      :digraph.add_edge(ac.graph, node, v, {:token, token})
     end
 
     v
+  end
+
+
+  def build_trie(ac, queue \\ [:root])
+
+  def build_trie(ac, []) do
+    nil
+  end
+
+  def build_trie(ac, [node|tail]) do
+    next_edges = token_edges_from_node(ac, node) # NotImplemented
+    next_nodes = Enum.map next_edges, fn(e) ->
+      {_edge, _v1, v2, {:token, token}} = :digraph.edge(ac.graph, e)
+      if node == :root do
+        set_failure_for(ac, v2, :root)
+      else
+        fail_node = compute_failure_for(ac, node, token)
+        set_failure_for(ac, v2, fail_node)
+      end
+      v2
+    end
+    build_trie(ac, tail ++ next_nodes)
+  end
+
+  def set_failure_for(ac, node, failure_node) do
+    :digraph.add_edge(ac.graph, node, failure_node, :failure)
+  end
+
+  def compute_failure_for(ac, :root, token) do
+    :root
+  end
+
+  def compute_failure_for(ac, node, token) do
+    case token_edge_from_node(ac, node, token) do
+      nil ->
+        next = failure_node_from_node(ac, node)
+        compute_failure_for(ac, next, token)
+      e ->
+        {_edge, _v1, v2, _token_label} = ac.graph |> :digraph.edge(e)
+        v2
+    end
+  end
+
+  def failure_node_from_node(ac, node) do
+    Enum.find :digraph.out_edges(ac.graph, node), fn(e) ->
+      case e do
+        {_edge, _v1, v2, :failure} -> true
+        _not_a_failure_edge -> false
+      end
+    end
+  end
+
+  @doc """
+  given a node, return all :token-type edges coming from this node
+  """
+  def token_edges_from_node(ac, node) do
+    ac.graph
+    |> :digraph.out_edges(node)
+    |> Enum.filter(fn(e) ->
+      {_edge, _v1, _v2, label} = ac.graph |> :digraph.edge(e)
+      case label do
+        {:token, t} -> true
+        _not_a_token_edge -> false
+      end
+    end)
+  end
+
+  @doc """
+  given a node and a token, find the :token-type edge coming out of this node
+  that is labelled by the given token.
+  """
+  def token_edge_from_node(ac, node, sought_token) do
+    ac.graph
+    |> :digraph.out_edges(node)
+    |> Enum.find(fn(e) ->
+      {_edge, _v1, _v2, label} = ac.graph |> :digraph.edge(e)
+      case label do
+        {:token, t} -> t == sought_token
+        _not_a_token_edge -> false
+      end
+    end)
+  end
+
+  # defp goto(ac, node, [target_token|rest]) do
+  #   #results = result(ac, node, results)
+
+  #   edges = :digraph.out_edges(ac.graph, node)
+  #   case Enum.find(edges, fn(e) -> edge_matches(ac, e, target_token) end) do
+  #     nil ->
+  #       if node == :root do
+  #         goto(ac, :root, rest)
+  #       else
+  #         fail(ac, node, [target_token|rest])
+  #       end
+  #     edge ->
+  #       {_edge, _v1, v2, _label} = :digraph.edge(ac.graph, edge)
+  #       goto(ac, v2, rest)
+  #   end
+  # end
+
+  # defp goto(ac, node, []) do
+  #   results
+  # end
+
+  # defp result(ac, node, results) do
+  #   # TODO. if the node has a result label, return (results + newresult)
+  # end
+
+  def search(ac, input) do
+    input_tokens = tokenize(input)
+    #results = goto(ac, :root, input_tokens, [])
+    #results
   end
 
   def num_nodes(ac) do
@@ -64,13 +177,16 @@ defmodule AhoCorasick do
   end
 
   def print(ac, node, level) do
+    if level == 0 do IO.write("\n") end
     IO.write("+ #{label ac, node}\n")
     Enum.each :digraph.out_edges(ac.graph, node), fn(e) ->
-      {_e, _v1, v2, l} = :digraph.edge(ac.graph, e)
-      IO.write("#{String.duplicate("  ", level)}|#{l}")
-      print(ac, v2, level + 1)
+      case :digraph.edge(ac.graph, e) do
+        {_e, _v1, v2, {:token, token}} ->
+          IO.write("#{String.duplicate("  ", level)}|#{token}")
+          print(ac, v2, level + 1)
+        _other_kind_of_edge -> nil
+      end
     end
-
   end
 
   def label(ac, node) do
@@ -78,17 +194,24 @@ defmodule AhoCorasick do
     label
   end
 
-  defp tokenize(term) do
+  @doc """
+  Splits a dictionary entry into tokens.
+
+  could be configurable in the future, but for now default is to tokenize per-
+  character. per-word or something could maybe save memory because less nodes,
+  but that means comparison on each node is now strcomp, partially thwarting
+  the value of aho-corasick itself.
+
+  also, this should be configurable w.r.t. case-sensitivity. For case-insensitive
+  searches, we should downcase everything here.
+  """
+  def tokenize(term) do
     String.split(term, "")
   end
 
   defp edge_matches(ac, edge, token) do
-    {_, _, _, l} = :digraph.edge(ac.graph, edge)
-    l == token
+    {_, _, _, {:token, label}} = :digraph.edge(ac.graph, edge)
+    label == token
   end
-
-  # defp goto(node, _) do
-
-  # end
 
 end
--- test/aho_corasick_test.exs
+++ test/aho_corasick_test.exs
@@ -3,15 +3,8 @@ defmodule AhoCorasickTest do
   import Debug
 
 
-  test "new" do
-    %AhoCorasick{graph: g} =  AhoCorasick.new()
-    t = elem(g, 0)
-    assert t == :digraph
-  end
-
   test "add a term" do
-    ag = AhoCorasick.new()
-    AhoCorasick.add_term(ag, "foo")
+    ag = AhoCorasick.new(["foo"])
     #debug :digraph.vertices(ag.graph)
     #debug :digraph.edges(ag.graph)
 
@@ -19,29 +12,21 @@ defmodule AhoCorasickTest do
     assert 4 == AhoCorasick.num_nodes(ag)
   end
 
-  test "add tokens should return the final node" do
-    ag = AhoCorasick.new()
-    terminus = ag |> AhoCorasick.add_tokens(["a", "b", "c"])
-
-    label = AhoCorasick.label(ag, terminus)
-
-    assert label == "c"
-  end
-
-
   test "add three terms" do
-    ag = AhoCorasick.new()
-    ag
-    |> AhoCorasick.add_term("foo")
-    |> AhoCorasick.add_term("for")
-    |> AhoCorasick.add_term("fall")
+    ag = AhoCorasick.new(["foo", "for", "fall"])
 
     AhoCorasick.print(ag)
 
     assert 8 == AhoCorasick.num_nodes(ag)
   end
 
+  test "add terms and find matches" do
+    ag = AhoCorasick.new(["he", "she", "his", "hers"])
 
+    AhoCorasick.print(ag)
 
+    # TODO search against "ushers"
+
+  end
 
 end
